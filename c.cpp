7f1bb92d387cb4ce19c57bcc03c32431d91e905b
AmanGitkumar <kumaraman55475@gmail.com>
2026-01-09 21:54:04 +0530

    done

--- c.cpp
+++ c.cpp
@@ -11,7 +11,15 @@
  */
 class Solution {
 public:
+    pair<int,TreeNode*> lca(TreeNode* root){
+            if(!root)return {0,NULL};
+            pair<int,TreeNode*> l = lca(root->left);
+            pair<int,TreeNode*> r = lca(root->right);
+            int cnt1 = l.first;
+            int cnt2 = r.first;
+            return {max(cnt1,cnt2)+1,cnt1 == cnt2 ? root: cnt1 > cnt2 ? l.second : r.second};
+    }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        // check
+        return lca(root).second;
     }
 };
